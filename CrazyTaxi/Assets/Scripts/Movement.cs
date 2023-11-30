0c5fa429adb892b1f170609a12dedc2e1717be82
Kenneth <alcanamay@gmail.com>
2023-11-30 00:46:06 -0600

    Cambiando cosas del movimiento

--- CrazyTaxi/Assets/Scripts/Movement.cs
+++ CrazyTaxi/Assets/Scripts/Movement.cs
@@ -7,12 +7,15 @@ public class Movement : MonoBehaviour
     [SerializeField] public float rotDegrees = 0.5f;
     [SerializeField] public Rigidbody cabinRigidbody;
     [SerializeField] private Vector3 relativeFwd;
-    [SerializeField] public float speed;
+    [SerializeField] public float acceleration;
+    [SerializeField] public float deacceleration;
     [SerializeField] private float maxSpeed;
+    [SerializeField] private float speed;
     public bool playerHasControl;
     public float minRotZ;
     public float maxRotZ;
     public float rotZ;
+    private float currentSpeed = 0.0f;
 
     void FixedUpdate()
     {
@@ -20,48 +23,53 @@ public class Movement : MonoBehaviour
         if (playerHasControl)
         {
             if (Input.GetKey("w"))
-                cabinRigidbody.transform.Rotate(-rotDegrees,0, rotZ);
+                cabinRigidbody.transform.Rotate(-rotDegrees, 0, rotZ);
 
-            if ( Input.GetKey("s"))
+            if (Input.GetKey("s"))
             {
-                cabinRigidbody.transform.Rotate(rotDegrees,0,rotZ);
+                cabinRigidbody.transform.Rotate(rotDegrees, 0, rotZ);
             }
 
             if (Input.GetKey("d"))
             {
-                cabinRigidbody.transform.Rotate(0, rotDegrees,rotZ);  
+                cabinRigidbody.transform.Rotate(0, rotDegrees, rotZ);
 
             }
 
             if (Input.GetKey("a"))
             {
-                cabinRigidbody.transform.Rotate(0, -rotDegrees,rotZ); 
+                cabinRigidbody.transform.Rotate(0, -rotDegrees, rotZ);
             }
 
             if (Input.GetKey("space"))
             {
-                cabinRigidbody.velocity = relativeFwd * speed * maxSpeed;
+                currentSpeed = Mathf.MoveTowards(currentSpeed, maxSpeed, acceleration * Time.fixedDeltaTime);
+                cabinRigidbody.velocity = relativeFwd * speed * currentSpeed;
             }
-           
-            if (Input.GetKeyUp("space"))
+            else
             {
-                cabinRigidbody.velocity = relativeFwd * speed * maxSpeed / 2;
+                currentSpeed = Mathf.MoveTowards(currentSpeed, 0, deacceleration * Time.fixedDeltaTime);
+                cabinRigidbody.velocity = relativeFwd * speed * currentSpeed;
             }
-            
+
             if (Input.GetKey("q"))
             {
-                cabinRigidbody.velocity = relativeFwd * speed * maxSpeed *-1;
+                currentSpeed = Mathf.MoveTowards(currentSpeed, maxSpeed, acceleration * Time.fixedDeltaTime);
+                cabinRigidbody.velocity = relativeFwd * speed * currentSpeed * -1;
             }
-           
+
             if (Input.GetKeyUp("q"))
             {
-                cabinRigidbody.velocity = relativeFwd * speed * maxSpeed *-1 / 2;
+                currentSpeed = Mathf.MoveTowards(currentSpeed, 0, deacceleration * Time.fixedDeltaTime);
+                cabinRigidbody.velocity = relativeFwd * speed * currentSpeed *-1;
             }
 
             if (Input.GetKey("e"))
             {
-                cabinRigidbody.velocity = Vector3.zero;
-            } 
+                currentSpeed = 0;
+                cabinRigidbody.velocity = relativeFwd * speed * currentSpeed;
+            }
         }
     }
 }
+
